568d1bc4cf8b1233184ee2ef66d2f14d43b8d9c9
nikhil-itd <nikhilgoudrukmammolla@gmail.com>
2025-06-10 23:24:20 +0530

    wip

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,7 +5,8 @@ import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/
 export default function Contact() {
   const [contactDialogOpen, setContactDialogOpen] = useState(false)
   return (
-    <section id="contact" className="py-20 bg-white text-gray-900 ">
+    <section id="contact" className="py-20 bg-white text-gray-900  ">
+      {/*  */}
       <div className="container mx-auto px-6 text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-6">Ready to Transform Your Hospital Pharmacy?</h2>
         <p className="text-lg md:text-xl max-w-2xl mx-auto mb-10">
